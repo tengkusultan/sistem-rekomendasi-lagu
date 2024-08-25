678276a1d9565699754b3b88d911b8d333f86262
tengkusultan <137712532+tengkusultan@users.noreply.github.com>
2024-08-25 21:40:27 +0700

    Update sistem.py

--- sistem.py
+++ sistem.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import nltk
+nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
